870c4d314c96d7f71af1d0a184b1d611f9d3447d
shakil islam <freelancershakil913@gmail.com>
2022-01-17 13:12:14 +0600

    first component done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
+
 require('./bootstrap');
+
+import Vue from 'vue';
+import mainapp from './components/mainapp.vue'
+
+Vue.component('mainapp', require('./components/mainapp.vue').default)
+
+const vue = new Vue({
+    el: '#app',
+
+    components:{
+     mainapp
+    }
+})
---
+++ resources/js/components/mainapp.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        First component here
+    </div>
+</template>
+
+
+<script type="text/babel">
+export default {
+
+    data() {
+        return{
+
+        }
+    },
+
+    mounted(){
+        console.log('component was run');
+    }
+}
+</script>
